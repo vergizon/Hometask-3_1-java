054a87700a7d6696a315fa78b75ecc5ccce9f7fe
Alex Vergizov <alvergizoff@gmail.com>
2021-07-28 23:14:39 +0300

    Main.java amended

--- Main.java
+++ Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        int K = 20; //количество рублей, за которую начисляется 1 миля
+        int k = 20; //количество рублей, за которую начисляется 1 миля
         int miles; //количество начисленных миль за купленный билет
         int ticketCost = 1019; //стоимость билета
 
-        miles = ticketCost / K;
+        miles = ticketCost / k;
 
         System.out.println(miles);
     }
